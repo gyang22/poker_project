4b45fdff89c7c616d1a75eeacd72a3161b9b8452
Grant Yang <gyang276@berkeley.edu>
2023-10-04 14:40:54 -0700

    predetermined dice

--- Dice.py
+++ Dice.py
@@ -11,3 +11,19 @@ class Dice:
     
     def roll(self):
         return random.randint(1, self.num_sides)
+
+
+"""
+Class representing a specialized die which only rolls in a specific order that is given on instantiation.
+"""
+class PredeterminedDice(Dice):
+
+    def __init__(self, roll_sequence, sides=6):
+        super().__init__(sides)
+        self.sequence = roll_sequence
+
+    
+    def roll(self):
+        while True:
+            for roll in self.sequence:
+                yield roll
